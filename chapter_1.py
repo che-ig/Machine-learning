c7d7392e9e0138ff7b10c36c934857d3ff0df819
che-ig <che@che-desktop>
2020-04-12 17:58:05 +0300

    добавил реализацию на нумпай

--- chapter_1.py
+++ chapter_1.py
@@ -1,3 +1,4 @@
+import numpy as np
 
 def w_sum(a, b):
     assert(len(a) == len(b))
@@ -6,10 +7,10 @@ def w_sum(a, b):
         output += (a[i] * b[i])
     return output
 
-weights = [0.1, 0.2, 0] 
-
+#weights = [0.1, 0.2, 0] 
+weights = np.array([0.1, 0.2, 0])
 def neural_network(input, weights):
-    prediction = w_sum(input, weights)
+    prediction = input.dot(weights) # произведение вектора строки на вектор столбец
     return prediction
 
 # This dataset is the current
@@ -21,14 +22,21 @@ def neural_network(input, weights):
 # wlrec = current games won (percent)
 # nfans = fan count (in millions)
 
-toes =  [8.5, 9.5, 9.9, 9.0]
-wlrec = [0.65, 0.8, 0.8, 0.9]
-nfans = [1.2, 1.3, 0.5, 1.0]
+toes =  np.array([8.5, 9.5, 9.9, 9.0])
+wlrec = np.array([0.65, 0.8, 0.8, 0.9])
+nfans = np.array([1.2, 1.3, 0.5, 1.0])
 
 # Input corresponds to every entry
 # for the first game of the season.
 
-input = [toes[0], wlrec[0], nfans[0]]
+input = np.array([toes[0], wlrec[0], nfans[0]])
 pred = neural_network(input, weights)
 
+'''
+def average(input):
+    summ = sum(input)
+    return summ / len(input)
+print(average(toes))
+'''
 print(pred)
+
