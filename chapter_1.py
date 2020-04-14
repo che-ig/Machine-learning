86abb0ab3a08efa394b59d543c56e55adcfc6a4e
che-ig <che@che-desktop>
2020-04-14 21:39:02 +0300

    Реализация сети без numPy

--- chapter_1.py
+++ chapter_1.py
@@ -8,29 +8,41 @@ def w_sum(a, b):
     return output
 
 #weights = [0.1, 0.2, 0] 
-weights = np.array([0.1, 0.2, 0])
+            #toes %win #fans
+ih_wgt = [ [0.1, 0.2, -0.1], #hid[0]
+           [-0.1,0.1, 0.9], #hid[1]
+           [0.1, 0.4, 0.1] ] #hid[2]
+
+           #hid[0] hid[1] hid[2]
+hp_wgt = [ [0.3, 1.1, -0.3], #hurt?
+           [0.1, 0.2, 0.0], #win?
+           [0.0, 1.3, 0.1] ] #sad?
+
+weights = [ih_wgt, hp_wgt]
+
+def vect_mat_mul(vect,matrix):
+    assert(len(vect) == len(matrix))
+    output = [0,0,0]
+    for i in range(len(vect)):
+        output[i] = w_sum(vect,matrix[i])
+    return output
+
 def neural_network(input, weights):
-    prediction = input.dot(weights) # произведение вектора строки на вектор столбец
-    return prediction
+    hid = vect_mat_mul(input,weights[0])
+    pred = vect_mat_mul(hid,weights[1])
+    return pred
 
-# This dataset is the current
-# status at the beginning of
-# each game for the first 4 games
-# in a season.
-
-# toes = current number of toes
-# wlrec = current games won (percent)
-# nfans = fan count (in millions)
-
-toes =  np.array([8.5, 9.5, 9.9, 9.0])
-wlrec = np.array([0.65, 0.8, 0.8, 0.9])
-nfans = np.array([1.2, 1.3, 0.5, 1.0])
+toes =  [8.5, 9.5, 9.9, 9.0]
+wlrec = [0.65,0.8, 0.8, 0.9]
+nfans = [1.2, 1.3, 0.5, 1.0]
 
 # Input corresponds to every entry
 # for the first game of the season.
 
-input = np.array([toes[0], wlrec[0], nfans[0]])
-pred = neural_network(input, weights)
+input = [toes[0],wlrec[0],nfans[0]]
+pred = neural_network(input,weights)
+
+print(pred)
 
 '''
 def average(input):
@@ -38,12 +50,13 @@ def average(input):
     return summ / len(input)
 print(average(toes))
 '''
-print(pred)
 
 # print(neural_network(np.array([toes[0], wlrec[0], nfans[0]]), np.array([0.1, 0.2, -0.1])))
+'''
 w1 = [toes[0], wlrec[0], nfans[0]]
 w2 = [0.1, 0.2, -0.1]
 def summ(e1, e2):
     return e1 * e2
 res = sum(list(map(summ, w1, w2)))
 print(res)
+'''
