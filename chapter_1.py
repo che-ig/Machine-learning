d734b72bb95e16c1c168e9cb3d281c003f29cb2e
che-ig <che@che-desktop>
2020-04-14 22:11:55 +0300

    Вариант на numPy

--- chapter_1.py
+++ chapter_1.py
@@ -9,14 +9,14 @@ def w_sum(a, b):
 
 #weights = [0.1, 0.2, 0] 
             #toes %win #fans
-ih_wgt = [ [0.1, 0.2, -0.1], #hid[0]
+ih_wgt = np.array([ [0.1, 0.2, -0.1], #hid[0]
            [-0.1,0.1, 0.9], #hid[1]
-           [0.1, 0.4, 0.1] ] #hid[2]
+           [0.1, 0.4, 0.1] ]) #hid[2]
 
            #hid[0] hid[1] hid[2]
-hp_wgt = [ [0.3, 1.1, -0.3], #hurt?
+hp_wgt = np.array([ [0.3, 1.1, -0.3], #hurt?
            [0.1, 0.2, 0.0], #win?
-           [0.0, 1.3, 0.1] ] #sad?
+           [0.0, 1.3, 0.1] ]) #sad?
 
 weights = [ih_wgt, hp_wgt]
 
@@ -28,18 +28,18 @@ def vect_mat_mul(vect,matrix):
     return output
 
 def neural_network(input, weights):
-    hid = vect_mat_mul(input,weights[0])
-    pred = vect_mat_mul(hid,weights[1])
+    hid = input.dot(weights[0])
+    pred = hid.dot(weights[1])
     return pred
 
-toes =  [8.5, 9.5, 9.9, 9.0]
-wlrec = [0.65,0.8, 0.8, 0.9]
-nfans = [1.2, 1.3, 0.5, 1.0]
+toes =  np.array([8.5, 9.5, 9.9, 9.0])
+wlrec = np.array([0.65,0.8, 0.8, 0.9])
+nfans = np.array([1.2, 1.3, 0.5, 1.0])
 
 # Input corresponds to every entry
 # for the first game of the season.
 
-input = [toes[0],wlrec[0],nfans[0]]
+input = np.array([toes[0],wlrec[0],nfans[0]])
 pred = neural_network(input,weights)
 
 print(pred)
