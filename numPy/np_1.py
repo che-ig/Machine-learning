a84dd9ed0123f699828efe99c7c4379b13bdfcbc
chernov.ig <test@example.com>
2020-10-13 22:32:48 +0300

    I added first strings

--- numPy/np_1.py
+++ numPy/np_1.py
@@ -1,5 +1,14 @@
 import numpy as np
+a = np.array([1, 8, 2, 4])
+# создание массива содержащий нули
+z = np.zeros((2, 2))
+# создание массива содержащий еденицы 
+o = np.ones(2, dtype=np.int64)
+# создание массива рандомных значений 
+e = np.empty(2)
 
-arr = np.array([1, 2])
-print(arr)
-
+# создание массива диапозона от нуля до требуемого значения 
+r = np.arange(4)
+print(np.sort(a))
+print(np.concatenate((e, o)))
+print(z, o, e, r)
