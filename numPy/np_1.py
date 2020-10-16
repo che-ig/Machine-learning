62cd6cd4e4edc7b66f9ccba67a9d89b30782307b
chernov.ig <test@example.com>
2020-10-16 17:16:44 +0300

    a basic functions

--- numPy/np_1.py
+++ numPy/np_1.py
@@ -1,14 +1,21 @@
 import numpy as np
-a = np.array([1, 8, 2, 4])
+simple_array = np.array([1, 8, 2, 4])
+array_example = np.array([[5,5], [6,6], [7,7]])
 # создание массива содержащий нули
 z = np.zeros((2, 2))
 # создание массива содержащий еденицы 
-o = np.ones(2, dtype=np.int64)
+o = np.ones((2, 4), dtype=np.int64)
 # создание массива рандомных значений 
 e = np.empty(2)
 
 # создание массива диапозона от нуля до требуемого значения 
 r = np.arange(4)
-print(np.sort(a))
-print(np.concatenate((e, o)))
-print(z, o, e, r)
+print(np.sort(simple_array))
+print(np.concatenate((e, simple_array)))
+print(z, o, e)
+# shape and size of array
+print(array_example.ndim)
+print(array_example.size)
+print(array_example.shape)
+# create a new form
+print(array_example.reshape(2, 3))
