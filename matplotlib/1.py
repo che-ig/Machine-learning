230b10e6e041bed4119ad5ec0139fb274623b0c1
chernov.ig <che_test@test>
2021-03-01 13:22:59 +0300

    эксперимент с линиями

--- matplotlib/1.py
+++ matplotlib/1.py
@@ -1,7 +1,7 @@
 from matplotlib import pyplot as plt
 import numpy as np
 
-x = np.linspace(0, 10, 50)
+x = np.linspace(0, 10, 30)
 y = x
 y2 = [i**2 for i in x]
 
@@ -9,24 +9,29 @@ y2 = [i**2 for i in x]
 plt.figure(figsize=(9, 9))
 
 plt.subplot(2, 2, 1)
-plt.plot(x, y)
+plt.plot(x, y, 'rx', label='first graph')
 plt.title('Linear dependence y = x')
 plt.ylabel('y', fontsize=14) # axis y
 plt.grid() # add grid 
+plt.legend()
 
 plt.subplot(2, 2, 2)
-plt.plot(x, y2) # create graph
-plt.xlabel('x') # axis x
+plt.plot(x, y2, 'g--') # create graph
+plt.xlabel('x', color='blue') # axis x
 plt.ylabel('y2', fontsize=14) # axis y
+plt.text(5, 80, 'my notice', color='red') # the text on the graph
 plt.grid() # add grid 
-plt.legend()
 
 plt.subplot(2, 2, 3)
 fruits = ['apple', 'peach', 'orange', 'bannana', 'melon']
 counts = [34, 25, 43, 31, 17] 
-plt.bar(fruits, counts)
+plt.bar(fruits, counts, label='All fruits')
 plt.title('Fruits')
-plt.xlabel('Count')
-plt.ylabel('fruit')
+plt.xlabel('Count', color='blue')
+plt.ylabel('fruit', color='blue')
+plt.legend()
 
 plt.show()
+
+fig, ax = plt.subplots(figsize=(8, 6))
+print(fig, '\n', ax)
