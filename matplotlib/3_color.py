a70b926b5b6012e52adcd7103fabfb6e597752a0
chernov.ig <che_test@test>
2021-03-04 19:37:46 +0300

    colorbar

--- matplotlib/3_color.py
+++ matplotlib/3_color.py
@@ -1,8 +1,15 @@
 import numpy as np
 from matplotlib import pyplot as plt
+from mpl_toolkits.axes_grid1.inset_locator import inset_axes
 
 np.random.seed(123)
 vals = np.random.randint(10, size=(7, 7))
-plt.pcolor(vals)
-plt.colorbar()
+# fig, ax = plt.subplots(figsize=(4, 4))
+# gr = ax.pcolor(vals) #  cmap=plt.get_cmap('viridis', 11)
+# axins = inset_axes(ax, width='7%', height='30%', loc='lower left',
+# bbox_to_anchor=(1.01, 0., 1, 1), bbox_transform=ax.transAxes, borderpad=0)
+# plt.colorbar(gr, ticks=[0, 5, 10], cax=axins, label='Value')
+
+plt.pcolor(vals, cmap=plt.get_cmap('viridis', 11))
+plt.colorbar(orientation='horizontal', shrink=0.9, extend='max', label='Value')
 plt.show()
