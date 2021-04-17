00f7a2c6995babccc92c8b3bacfdbb498b3898ae
chernov.ig <che_test@test>
2021-04-17 14:42:29 +0300

    examples from 15

--- dlForKettle/2.py
+++ dlForKettle/2.py
@@ -1,12 +1,39 @@
-import numpy as np
+import turtle
+import math
 
-a1 = np.identity(3)
-a2 = np.eye(3)
-print(a1, '\n', a2)
+def poligon(t, n, length):
+    angle = 360 / n
+    poliline(t, n, length, angle)
 
-def recurse(n, s):
-    if n == 0:
-        print(s)
-    else:
-        recurse(n-1, n+s)
-recurse(800, 0)
+def circle(t, r):
+    c = 2 * math.pi * r
+    n = int(c / 3) + 1 # количество углов в многоугольнике
+    length = c / n
+    poligon(t, n, length)
+
+def poliline(t, n, length, angle):
+    for i in range(n):
+        t.fd(length)
+        t.lt(angle)
+
+
+
+bob = turtle.Turtle()
+# bob.fd(100)
+# bob.lt(90)
+# bob.fd(100)
+# bob.circle(50, 270, 27)
+
+
+# for x in range(18):
+#     bob.fd(200)
+#     bob.lt(170)
+
+
+poliline(bob, 30, 10, 2)
+circle(bob, 150)
+poligon(bob, 8, 100)
+
+turtle.mainloop()
+# bob.lt(80)
+# bob.fd(100)
---
+++ dlWitoutMath/chapter-15.py
@@ -0,0 +1,40 @@
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+import seaborn as sns ; sns.set()
+from sklearn import linear_model
+
+np.random.seed(42)
+ridge_estimator = linear_model.Ridge()
+
+num_pts = 100
+nouse_range = 0.2
+x_vals = []
+y_vals = []
+(x_left, x_right) = (-2, 2)
+for i in range(num_pts):
+    x = np.random.uniform(x_left, x_right)
+    y = np.random.uniform(-nouse_range, nouse_range) + (2 * math.sin(x))
+
+    x_vals.append(x)
+    y_vals.append(y)
+
+x_column = np.reshape(x_vals, (len(x_vals), 1))
+
+plt.figure(figsize=(10, 4))
+
+ridge_estimator.fit(x_column, y_vals)
+
+y_left = ridge_estimator.predict(np.array([[x_left]]))
+y_right = ridge_estimator.predict(np.reshape([x_right], [-1, 1]))
+
+plt.subplot(1, 2, 1)
+plt.scatter(x_vals, y_vals)
+plt.plot([x_left, x_right], [y_left, y_right], 'o-.r')
+plt.title('original data with best line')
+
+plt.subplot(1, 2, 2)
+plt.scatter(x_vals, y_vals)
+plt.title('original data')
+
+plt.show()
