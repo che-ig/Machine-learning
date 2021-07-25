67c625b42570f792fcc3bd171904fbb8cc896871
chernov.ig <che_test@test>
2021-07-25 18:02:27 +0300

    erotosfen

--- basic_python/nod_and_Eratosfen.py
+++ basic_python/nod_and_Eratosfen.py
@@ -4,7 +4,21 @@ def nod(a, b):
     else:
         return nod(b, a % b)
 
+def erotosfen(a):
+    primes = []
+    arr = [True] * a
+    arr[0], arr[1] = False, False
+    for i in range(2, a):
+        if arr[i]:
+            for j in range(i*2, a, i):
+                arr[j] = False
+    
+    return [v for v, _ in filter(lambda x: x[1], enumerate(arr))]
+                
 
 if __name__ == '__main__':
-    r = nod(4, 16)
+    r = nod(116, 117)
     print(r)
+
+    prime_numbers = erotosfen(144)
+    print(prime_numbers)
