3bfe20ee7cc2d69e7f6689b50a559073afe57eea
chernov.ig <mr.ivan.chernov@yandex.ru>
2024-07-03 16:38:05 +0300

    поправил несколько замечаний и добавил новый файл для pre-commit

--- chapter_6.py
+++ chapter_6.py
@@ -1,22 +1,22 @@
 import numpy as np
-weights = np.array([0.5,0.48,-0.7])
+weights = np.array([0.5, 0.48, -0.7])
 alpha = 0.1
 
-streetlights = np.array( 
-   [ 
-      [ 1, 0, 1 ],
-      [ 0, 1, 1 ],
-      [ 0, 0, 1 ],
-      [ 1, 1, 1 ],
-      [ 0, 1, 1 ],
-      [ 1, 0, 1 ] 
-   ] 
+streetlights = np.array(
+   [
+      [1, 0, 1],
+      [0, 1, 1],
+      [0, 0, 1],
+      [1, 1, 1],
+      [0, 1, 1],
+      [1, 0, 1]
+   ]
 )
 
-walk_vs_stop = np.array( [ 0, 1, 0, 1, 1, 0 ] )
+walk_vs_stop = np.array([0, 1, 0, 1, 1, 0])
 
-input = streetlights[0] # [1,0,1]
-goal_prediction = walk_vs_stop[0] # equals 0... i.e. "stop"
+input = streetlights[0]  # [1,0,1]
+goal_prediction = walk_vs_stop[0]  # equals 0... i.e. "stop"
 
 for iteration in range(40):
     error_for_all_lights = 0
@@ -25,11 +25,11 @@ for iteration in range(40):
         goal_prediction = walk_vs_stop[row_index]
         print('weights => ', weights, '   input => ', input)
         prediction = input.dot(weights)
-        
+
         error = (goal_prediction - prediction) ** 2
         error_for_all_lights += error
-        
+
         delta = prediction - goal_prediction
-        weights = weights - (alpha * (input * delta))	
+        weights = weights - (alpha * (input * delta))
         print("Prediction:" + str(prediction), '  ', weights)
     print("Error:" + str(error_for_all_lights) + "\n")
---
+++ experiment.py
@@ -0,0 +1,8 @@
+import sys
+import pandas as pd
+import numpy as np
+
+print(pd.__version__, np.version.version)
+a = 2
+
+print(f'version python is -> {sys.version}')
