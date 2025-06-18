6d79f56f1d94540b170629b837d1f7fb2b1e52e5
chernov.ig <mr.ivan.chernov@yandex.ru>
2025-06-18 17:01:19 +0300

    добавил конфиг для yazi

---
+++ yazi/init.lua
@@ -0,0 +1,40 @@
+-- https://yazi-rs.github.io/docs/tips
+Status:children_add(function()
+    local h = cx.active.current.hovered
+    if not h or ya.target_family() ~= "unix" then
+        return ""
+    end
+
+    return ui.Line {
+        ui.Span(ya.user_name(h.cha.uid) or tostring(h.cha.uid)):fg("magenta"),
+        ":",
+        ui.Span(ya.group_name(h.cha.gid) or tostring(h.cha.gid)):fg("magenta"),
+        " "
+    }
+end, 500, Status.RIGHT)
+
+-- https://yazi-rs.github.io/docs/tips
+Header:children_add(function()
+    if ya.target_family() ~= "unix" then
+        return ""
+    end
+    return ui.Span(ya.user_name() .. "@" .. ya.host_name() .. ":"):fg("blue")
+end, 500, Header.LEFT)
+
+-- https://github.com/dedukun/bookmarks.yazi
+require("bookmarks"):setup({
+    last_directory = {enable = false, persist = false, mode = "dir"},
+    persist = "all",
+    desc_format = "parent",
+    file_pick_mode = "hover",
+    custom_desc_input = false,
+    notify = {
+        enable = false,
+        timeout = 1,
+        message = {
+            new = "New bookmark '<key>' -> '<folder>'",
+            delete = "Deleted bookmark in '<key>'",
+            delete_all = "Deleted all bookmarks"
+        }
+    }
+})
